c9ab24af4d2c20bb29de7c9f5f4051d26992324b
leona <leonardovieiraxy@hotmail.com>
2023-10-09 17:21:50 -0300

    Changes to be committed: 	new file:   U02/T01/exemplo.html 	new file:   U02/T01/vue-project/.gitignore 	new file:   U02/T01/vue-project/.vscode/extensions.json 	new file:   U02/T01/vue-project/README.md 	new file:   U02/T01/vue-project/index.html 	new file:   U02/T01/vue-project/package-lock.json 	new file:   U02/T01/vue-project/package.json 	new file:   U02/T01/vue-project/public/favicon.ico 	new file:   U02/T01/vue-project/src/App.vue 	new file:   U02/T01/vue-project/src/App1.vue 	new file:   U02/T01/vue-project/src/App2.vue 	new file:   U02/T01/vue-project/src/App3.vue 	new file:   U02/T01/vue-project/src/assets/base.css 	new file:   U02/T01/vue-project/src/assets/logo.svg 	new file:   U02/T01/vue-project/src/assets/main.css 	new file:   U02/T01/vue-project/src/components/HelloWorld.vue 	new file:   U02/T01/vue-project/src/components/TaskItem.vue 	new file:   U02/T01/vue-project/src/components/TaskList.vue 	new file:   U02/T01/vue-project/src/components/TheWelcome.vue 	new file:   U02/T01/vue-project/src/components/WelcomeItem.vue 	new file:   U02/T01/vue-project/src/components/icons/IconCommunity.vue 	new file:   U02/T01/vue-project/src/components/icons/IconDocumentation.vue 	new file:   U02/T01/vue-project/src/components/icons/IconEcosystem.vue 	new file:   U02/T01/vue-project/src/components/icons/IconSupport.vue 	new file:   U02/T01/vue-project/src/components/icons/IconTooling.vue 	new file:   U02/T01/vue-project/src/main.js 	new file:   U02/T01/vue-project/vite.config.js

---
+++ U02/T01/vue-project/src/App1.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <!-- Header compartilhado -->
+    <header>
+      <h1>Meu Aplicativo</h1>
+    </header>
+
+    <!-- Conteúdo variável -->
+    <main>
+      <!-- Botão com ação e conteúdo diferentes -->
+      <button @click="mostrarMensagem">Clique-me</button>
+
+      <!-- Input de formulário com variações de valores e atributos -->
+      <input :type="inputType" :placeholder="inputPlaceholder" v-model="inputValue" />
+
+      <!-- Exibição condicional de texto -->
+      <p v-if="mostrarTexto">{{ texto }}</p>
+    </main>
+
+    <!-- Footer compartilhado -->
+    <footer>
+      <p>Rodapé comum a todas as páginas</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputType: 'text',
+      inputPlaceholder: 'Digite algo...',
+      inputValue: '',
+      mostrarTexto: false,
+      texto: 'Texto a ser exibido',
+    };
+  },
+  methods: {
+    mostrarMensagem() {
+      this.mostrarTexto = !this.mostrarTexto;
+    },
+  },
+};
+</script>
+
+<style>
+/* Estilos específicos para o header, main e footer */
+header {
+  background-color: #333;
+  color: white;
+  text-align: center;
+  padding: 1rem;
+}
+
+main {
+  padding: 1rem;
+}
+
+footer {
+  background-color: #333;
+  color: white;
+  text-align: center;
+  padding: 1rem;
+}
+</style>
