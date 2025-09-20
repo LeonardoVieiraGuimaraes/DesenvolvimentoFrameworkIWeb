0246a9d05221b942acad05d633f3c1d2bfe47292
LeonardoVieiraGuimaraes <leonardovieiraxy@hotmail.com>
2025-09-20 15:58:32 -0300

    git push

---
+++ U02/meu_primeiro_projeto/src/App.vue
@@ -0,0 +1,68 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <button class="button is-primary">
+  Button
+</button>
+<button class="button is-link">
+  Button
+</button>
+<button class="button is-info">
+  Button
+</button>
+<button class="button is-success">
+  Button
+</button>
+<button class="button is-warning">
+  Button
+</button>
+<button class="button is-danger">
+  Button
+</button>
+Leonardo  Vieira Guimarães
+
+<input class="input" type="text" placeholder="Text input">
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
